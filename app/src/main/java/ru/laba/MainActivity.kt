ec2cf147cc139c17eecd974dd242ef6ff1a78d56
DrStrange1224 <maksim.petr0w@mail.ru>
2026-02-10 12:28:49 +0400

    feat: init commit

--- app/src/main/java/ru/laba/MainActivity.kt
+++ app/src/main/java/ru/laba/MainActivity.kt
@@ -35,201 +35,21 @@ import androidx.compose.ui.unit.sp
 import ru.laba.ui.theme.LabaTheme
 
 class MainActivity : ComponentActivity() {
-    var isTablet : Boolean = false
 
+    //START
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            isTablet = LocalContext.current.resources.getBoolean(R.bool.isTablet)
-            InitLayout(
-                orientation=resources.configuration.orientation,
-                isTablet=isTablet
-            )
+
         }
     }
 
+    //for checking if orientation changed
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         setContent {
-            InitLayout(
-                orientation=resources.configuration.orientation,
-                isTablet=isTablet
-            )
+
         }
-    }
-}
-
-@Composable
-fun InitLayout(orientation: Int, isTablet : Boolean){
-    LabaTheme{
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            containerColor = MaterialTheme.colorScheme.background
-        ){
-            innerPadding -> run {
-                when (orientation){
-                    Configuration.ORIENTATION_PORTRAIT -> {
-                        if (isTablet){
-                            PortraitTabletLayout(innerPadding)
-                        }else{
-                            PortraitPhoneLayout(innerPadding)
-                        }
-                    }
-
-                    Configuration.ORIENTATION_LANDSCAPE -> {
-                        if (isTablet){
-                            LandscapeTabletLayout(innerPadding)
-                        }else{
-                            LandscapePhoneLayout(innerPadding)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun LandscapePhoneLayout(innerPadding : PaddingValues){
-    Row(
-        modifier = Modifier.fillMaxSize().padding(innerPadding),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Spacer(Modifier.weight(1f))
-        IconFrame(Modifier.weight(4f))
-        Spacer(Modifier.weight(1f))
-        ContactsFrame(Modifier.weight(3f))
-    }
-}
-
-@Composable
-fun PortraitPhoneLayout(innerPadding : PaddingValues){
-    Column(
-        modifier = Modifier.fillMaxSize().padding(innerPadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Spacer(Modifier.weight(2f))
-        IconFrame(Modifier.weight(4f))
-        Spacer(Modifier.weight(1f))
-        ContactsFrame(Modifier.weight(3f))
-    }
-}
-
-@Composable
-fun LandscapeTabletLayout(innerPadding : PaddingValues){
-    Row(
-        modifier = Modifier.fillMaxSize().padding(innerPadding),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Spacer(Modifier.weight(1f))
-        IconFrame(Modifier.weight(4f))
-        Spacer(Modifier.weight(1f))
-        ContactsFrame(Modifier.weight(3f))
-    }
-}
-
-@Composable
-fun PortraitTabletLayout(innerPadding : PaddingValues){
-    Column(
-        modifier = Modifier.fillMaxSize().padding(innerPadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Spacer(Modifier.weight(1f))
-        IconFrame(Modifier.weight(4f))
-        Spacer(Modifier.weight(1f))
-        ContactsFrame(Modifier.weight(3f))
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun IconFramePreview(){
-    LabaTheme {
-        IconFrame()
-    }
-}
-
-@Composable
-fun IconFrame(modifier : Modifier = Modifier){
-    Column(modifier = modifier) {
-        Image(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            painter = painterResource(R.drawable.app_logo),
-            contentDescription = ""
-        )
-        Spacer(
-            Modifier.height(dimensionResource(R.dimen.icon_incol_space))
-        )
-        Text(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            text = stringResource(R.string.icon_dev_name),
-            fontSize = dimensionResource(R.dimen.icon_dev_name_fontsz).value.sp,
-            color = MaterialTheme.colorScheme.primary,
-        )
-        Spacer(
-            Modifier.height(dimensionResource(R.dimen.icon_incol_space))
-        )
-        Text(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            text = stringResource(R.string.icon_group_num),
-            fontSize = dimensionResource(R.dimen.icon_group_num_fontsz).value.sp,
-            color = MaterialTheme.colorScheme.secondary,
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ContactsFramePreview(){
-    LabaTheme{
-        ContactsFrame()
-    }
-}
-
-@Composable
-fun ContactsFrame(modifier : Modifier = Modifier){
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.Start
-    ) {
-        ContactRow(
-            imgRes = R.drawable.phone_icon,
-            textRes = R.string.contact_phone
-        )
-        Spacer(
-            Modifier.height(dimensionResource(R.dimen.contacts_incol_space))
-        )
-        ContactRow(
-            imgRes = R.drawable.tg_icon,
-            textRes = R.string.contact_tg
-        )
-        Spacer(
-            Modifier.height(dimensionResource(R.dimen.contacts_incol_space))
-        )
-        ContactRow(
-            imgRes = R.drawable.email_icon,
-            textRes = R.string.contact_email
-        )
-    }
-}
-
-@Composable
-fun ContactRow(@DrawableRes imgRes : Int = R.drawable.app_logo, @StringRes textRes : Int = R.string.default_str){
-    Row{
-        Image(
-            modifier = Modifier.width(dimensionResource(R.dimen.contacts_icon_width)).align(Alignment.CenterVertically),
-            painter = painterResource(imgRes),
-            contentDescription = ""
-        )
-        Spacer(
-            Modifier.width(dimensionResource(R.dimen.contacts_inrow_space))
-        )
-        Text(
-            modifier = Modifier.align(Alignment.CenterVertically),
-            text = stringResource(textRes),
-            fontSize = dimensionResource(R.dimen.contacts_fontsz).value.sp,
-            color = MaterialTheme.colorScheme.primary,
-        )
     }
 }
--- app/src/main/java/ru/laba/ui/theme/Theme.kt
+++ app/src/main/java/ru/laba/ui/theme/Theme.kt
@@ -41,16 +41,12 @@ fun LabaTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-//    val colorScheme = when {
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
-//
-//        darkTheme -> DarkColorScheme
-//        else -> LightColorScheme
-//    }
-    val colorScheme = when{
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
